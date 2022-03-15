01a264e0ed8d2715658245f4bb52dce3583e4a70
sujeetgh <sujeetmehta6@gmail.com>
2022-03-15 17:04:26 +0530

    added absolute path for img

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 
 load_dotenv()
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates("templates/")
 
 @app.get("/")
