190281c48edebebb45da051d8fd8351c51f3542f
sujeetgh <sujeetmehta6@gmail.com>
2022-03-15 17:18:11 +0530

    debug

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 
 load_dotenv()
 app = FastAPI()
 templates = Jinja2Templates("templates/")
+app.mount("/static", StaticFiles(directory="static/"), "static")
+
 
 @app.get("/")
 async def home(request: Request):
